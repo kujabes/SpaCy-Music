79f344977e6451689eaae56070f07474252ee367
Zyan Karim <zyan.karim@gmail.com>
2024-03-28 01:13:44 -0500

    Added New Key Profile Types

--- generate_profiles.py
+++ generate_profiles.py
@@ -1,18 +1,45 @@
 from music21 import scale
 import json
 
-CENTERS = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+# The 12 unique pitch classes
+# Note: Some notes have two names (ex. C# is the Same as D-). 
+# to avoid duplicates and for the sake of consistency
+# the choice was made to use the sharp representation
+# for these notes
+PITCH_CLASSES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
+# Given a scale, create a key profile 
+# representing that scale
 def profile_generator(scale):
     profile = [0] * 12
-    for i, note in enumerate(CENTERS):
+    for i, note in enumerate(PITCH_CLASSES):
         if note in scale:
             profile[i] = 1
     
     return profile
 
+def neg_bias_profile_generator(scale):
+    profile = [-1] * 12
+    for i, note in enumerate(PITCH_CLASSES):
+        if note in scale:
+            profile[i] = 1
+    
+    return profile
+
+def root_bias_profile_generator(scale, root):
+    profile = [0] * 12
+    for i, note in enumerate(PITCH_CLASSES):
+        if note in scale:
+            if note == root:
+                profile[i] = 2
+            else:   
+                profile[i] = 1
+    
+    return profile
 
 def main():
+    # Initializing the 8 modes, of which there are 12 scales
+    # Note: not all 72 scales are unique. (Ex. Cmaj == Amin)
     major = dict(list())
     minor = dict(list())
     mixo = dict(list())
@@ -21,6 +48,7 @@ def main():
     lydian = dict(list())
     locrian = dict(list())
 
+    # Mapping to rename flat notes into their sharp equivalent
     flat_map = {
             "C-": "B",
             "D-": "C#",
@@ -30,17 +58,16 @@ def main():
             "A-": "G#",
             "B-": "A#"
         }
-    for center in CENTERS:
-        
-
-        # All modes for a given note
-        maj_scale = [str(note)[:-1] for note in scale.MajorScale(center).getPitches()]
-        min_scale = [str(note)[:-1] for note in scale.MinorScale(center).getPitches()]
-        mixo_scale = [str(note)[:-1] for note in scale.MixolydianScale(center).getPitches()]
-        dorian_scale = [str(note)[:-1] for note in scale.DorianScale(center).getPitches()]
-        phrygian_scale = [str(note)[:-1] for note in scale.PhrygianScale(center).getPitches()]
-        lydian_scale = [str(note)[:-1] for note in scale.LydianScale(center).getPitches()]
-        locrian_scale = [str(note)[:-1] for note in scale.LocrianScale(center).getPitches()]
+    
+    for pitch in PITCH_CLASSES:
+        # grabbing the scales for each mode of a given pitch class / note
+        maj_scale = [str(note)[:-1] for note in scale.MajorScale(pitch).getPitches()]
+        min_scale = [str(note)[:-1] for note in scale.MinorScale(pitch).getPitches()]
+        mixo_scale = [str(note)[:-1] for note in scale.MixolydianScale(pitch).getPitches()]
+        dorian_scale = [str(note)[:-1] for note in scale.DorianScale(pitch).getPitches()]
+        phrygian_scale = [str(note)[:-1] for note in scale.PhrygianScale(pitch).getPitches()]
+        lydian_scale = [str(note)[:-1] for note in scale.LydianScale(pitch).getPitches()]
+        locrian_scale = [str(note)[:-1] for note in scale.LocrianScale(pitch).getPitches()]
 
         scales = [
             maj_scale,
@@ -53,6 +80,7 @@ def main():
         ]
 
         # normalizing scales so that flats appear as sharps
+        # note: relies on mutability of lists
         for s in scales:
             for i, note in enumerate(s):
                 if note in flat_map:
@@ -60,43 +88,44 @@ def main():
 
 
         # generating corresponding profiles
-        major_profile = profile_generator(maj_scale)
-        minor_profile = profile_generator(min_scale)
-        mixo_profile = profile_generator(mixo_scale)
-        dorian_profile = profile_generator(dorian_scale)
-        phrygian_profile = profile_generator(phrygian_scale)
-        lydian_profile = profile_generator(lydian_scale)
-        locrian_profile = profile_generator(locrian_scale)
+        minor_profile = root_bias_profile_generator(min_scale, pitch)
+        major_profile = root_bias_profile_generator(maj_scale, pitch)
+        mixo_profile = root_bias_profile_generator(mixo_scale, pitch)
+        dorian_profile = root_bias_profile_generator(dorian_scale, pitch)
+        phrygian_profile = root_bias_profile_generator(phrygian_scale, pitch)
+        lydian_profile = root_bias_profile_generator(lydian_scale, pitch)
+        locrian_profile = root_bias_profile_generator(locrian_scale, pitch)
 
-        # creating dictionary for each profile
-        major[center] = major_profile
-        minor[center] = minor_profile
-        mixo[center] = mixo_profile
-        dorian[center] = dorian_profile
-        phrygian[center] = phrygian_profile
-        lydian[center] = lydian_profile
-        locrian[center] = locrian_profile
+        # creating a mapping for each profile
+        major[pitch] = major_profile
+        minor[pitch] = minor_profile
+        mixo[pitch] = mixo_profile
+        dorian[pitch] = dorian_profile
+        phrygian[pitch] = phrygian_profile
+        lydian[pitch] = lydian_profile
+        locrian[pitch] = locrian_profile
 
     # dumping all the dictionaries into json files
-    with open("key_profiles/major.json", "w") as f:
+    path = "root_bias_key_profiles"
+    with open(f"{path}/major.json", "w") as f:
         json.dump(major, f)
 
-    with open("key_profiles/minor.json", "w") as f:
+    with open(f"{path}/minor.json", "w") as f:
         json.dump(minor, f)
 
-    with open("key_profiles/mixolydian.json", "w") as f:
+    with open(f"{path}/mixolydian.json", "w") as f:
         json.dump(mixo, f)
 
-    with open("key_profiles/dorian.json", 'w') as f:
+    with open(f"{path}/dorian.json", 'w') as f:
         json.dump(dorian, f)
 
-    with open("key_profiles/phrygian.json", 'w') as f:
+    with open(f"{path}/phrygian.json", 'w') as f:
         json.dump(phrygian, f)
     
-    with open("key_profiles/lydian.json", 'w') as f:
+    with open(f"{path}/lydian.json", 'w') as f:
         json.dump(lydian, f)
     
-    with open("key_profiles/locrian.json", 'w') as f:
+    with open(f"{path}/locrian.json", 'w') as f:
         json.dump(locrian, f)
 
 
